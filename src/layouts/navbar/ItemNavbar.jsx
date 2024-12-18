47ee6694fc9b7b230839ba1b9e81fd5cfecf93cd
Jomarth <117119030+Jomarth@users.noreply.github.com>
2024-12-18 10:23:03 -0600

    Update ItemNavbar.jsx

--- src/layouts/navbar/ItemNavbar.jsx
+++ src/layouts/navbar/ItemNavbar.jsx
@@ -22,7 +22,7 @@ function ItemNavbar({trigger, links}) {
                     links.map((link) => {
                         return (
                             <MenuItem key={link?.name}>
-                                <button onClick={navigate(link?.href + trigger.toLowerCase())}
+                                <button onClick={() => {navigate(link?.href + trigger.toLowerCase())}}
                                     className="block px-4 py-2 text-sm text-gray-700 data-[focus]:bg-gray-100 data-[focus]:outline-none"
                                     >
                                 {link?.name}
